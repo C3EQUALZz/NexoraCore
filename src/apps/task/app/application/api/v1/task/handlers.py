501d841b705090aaadd87c9bb6942dd29fb27add
matebookWindows <dan.kovalev2013@gmail.com>
2025-03-24 17:09:13 +0300

    refactored handlers

--- src/apps/task/app/application/api/v1/task/handlers.py
+++ src/apps/task/app/application/api/v1/task/handlers.py
@@ -7,7 +7,7 @@ from fastapi import APIRouter, HTTPException
 
 from app.application.api.v1.task.schemas import CreateTaskSchemaRequest, UpdateTaskSchemaRequest, TaskSchemaResponse
 from app.domain.entities.task import TaskEntity
-from app.exceptions.base import ApplicationException
+from app.exceptions.base import BaseAppException
 from app.infrastructure.uow.task.base import TasksUnitOfWork
 from app.logic.bootstrap import Bootstrap
 from app.logic.commands.tasks import DeleteTaskCommand, CreateTaskCommand, UpdateTaskCommand, UpdateTaskStatusCommand
@@ -24,20 +24,16 @@ logger = logging.getLogger(__name__)
 
 
 @router.get(
-    "/{task_id}",
+    "/{task_id}/",
     status_code=200,
 )
 async def get_task_by_id(
         uow: FromDishka[TasksUnitOfWork],
         task_id: UUID
 ) -> TaskSchemaResponse:
-    try:
-        tasks_view: TasksView = TasksView(uow=uow)
-        task: TaskEntity = await tasks_view.get_task_by_id(str(task_id))
-        return TaskSchemaResponse.from_entity(task)
-    except ApplicationException as e:
-        logger.error(e)
-        raise HTTPException(status_code=e.status, detail=str(e))
+    tasks_view: TasksView = TasksView(uow=uow)
+    task: TaskEntity = await tasks_view.get_task_by_id(str(task_id))
+    return TaskSchemaResponse.from_entity(task)
 
 
 @router.post("/")
@@ -45,45 +41,36 @@ async def create_task(
         schema: CreateTaskSchemaRequest,
         bootstrap: FromDishka[Bootstrap[TasksUnitOfWork]]
 ):
-    try:
-        messagebus: MessageBus = await bootstrap.get_messagebus()
-        await messagebus.handle(CreateTaskCommand(**schema.model_dump()))
-        return TaskSchemaResponse.from_entity(messagebus.command_result)
-    except ApplicationException as e:
-        logger.error(e)
-        raise HTTPException(status_code=e.status, detail=str(e))
+    messagebus: MessageBus = await bootstrap.get_messagebus()
+    await messagebus.handle(CreateTaskCommand(**schema.model_dump()))
+    return TaskSchemaResponse.from_entity(messagebus.command_result)
 
 
-@router.put("/{task_id}")
+@router.put("/{task_id}/")
 async def update_task(
         task_id: UUID,
         bootstrap: FromDishka[Bootstrap[TasksUnitOfWork]],
         schema: UpdateTaskSchemaRequest
 ) -> TaskSchemaResponse:
-    try:
-        messagebus: MessageBus = await bootstrap.get_messagebus()
-        await messagebus.handle(UpdateTaskCommand(**{"task_id": task_id, **schema.model_dump()}))
-        return TaskSchemaResponse.from_entity(messagebus.command_result)
-    except ApplicationException as e:
-        logger.error(e)
-        raise HTTPException(status_code=e.status, detail=str(e))
+    messagebus: MessageBus = await bootstrap.get_messagebus()
+    await messagebus.handle(UpdateTaskCommand(**{"task_id": task_id, **schema.model_dump()}))
+    return TaskSchemaResponse.from_entity(messagebus.command_result)
 
 
-@router.delete("/{task_id}")
+@router.delete(
+    "/{task_id}/",
+    description="Handler for deleting task by id"
+)
 async def delete_task(
         task_id: UUID,
         bootstrap: FromDishka[Bootstrap[TasksUnitOfWork]]
 ) -> None:
-    try:
-        messagebus: MessageBus = await bootstrap.get_messagebus()
-        await messagebus.handle(DeleteTaskCommand(oid=str(task_id)))
-        return messagebus.command_result
-    except ApplicationException as e:
-        logger.error(e)
-        raise HTTPException(status_code=e.status, detail=str(e))
+    messagebus: MessageBus = await bootstrap.get_messagebus()
+    await messagebus.handle(DeleteTaskCommand(oid=str(task_id)))
+    return messagebus.command_result
 
 
-@router.patch("/{task_id}/status")
+@router.patch("/{task_id}/status/")
 async def change_status_for_task(
         task_id: UUID,
         status: Literal["open", "in_progress", "completed"],
@@ -93,18 +80,18 @@ async def change_status_for_task(
         messagebus: MessageBus = await bootstrap.get_messagebus()
         await messagebus.handle(UpdateTaskStatusCommand(task_id=str(task_id), status=status))
         return TaskSchemaResponse.from_entity(messagebus.command_result)
-    except ApplicationException as e:
+    except BaseAppException as e:
         logger.error(e)
         raise HTTPException(status_code=e.status, detail=str(e))
 
 
-@router.post("/{task_id}/comments")
+@router.post("/{task_id}/comments/")
 async def create_comment_for_task(
         task_id: UUID,
         comment: str
 ):
     try:
         ...
-    except ApplicationException as e:
+    except BaseAppException as e:
         logger.error(e)
         raise HTTPException(status_code=e.status, detail=str(e))
